c2f5d8a597f4760b26e1ed3e2535c5f6ee36a52f
Hayden Erickson <haydenericksonband@gmail.com>
2023-12-14 19:52:23 -1000

    First commit

---
+++ app/routes/webhooks.tsx
@@ -0,0 +1,35 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+import db from "../db.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { topic, shop, session } = await authenticate.webhook(
+    request
+  );
+
+  // if (!admin) {
+    // The admin context isn't returned if the webhook fired after a shop was uninstalled.
+    // throw new Response();
+  // }
+
+  console.log(topic)
+
+  switch (topic) {
+    case "APP_UNINSTALLED":
+      if (session) {
+        await db.session.deleteMany({ where: { shop } });
+      }
+
+      break;
+    case "CUSTOMERS_DATA_REQUEST":
+    case "CUSTOMERS_REDACT":
+    case "SHOP_REDACT":
+    case "PRODUCTS_UPDATE":
+      console.log(request)
+      break
+    default:
+      throw new Response("Unhandled webhook topic", { status: 404 });
+  }
+
+  throw new Response();
+};
