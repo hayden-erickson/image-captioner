378fb6f117c25070ebac5e7939cad1f6c2b6702c
Hayden Erickson <haydenericksonband@gmail.com>
2024-02-27 11:17:19 -1000

    Create and delete webhook subscriptions on user action
    Using the shopify API, create or delete webhook subscriptions for
    product update and product create if the user enables/disables automatic
    image descriptions.

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -25,10 +25,13 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     case "CUSTOMERS_DATA_REQUEST":
     case "CUSTOMERS_REDACT":
     case "SHOP_REDACT":
+    case "PRODUCTS_CREATE":
+      console.log(request)
+      break
+
     case "PRODUCTS_UPDATE":
       // TODO if we update a product after a product update webhook will it
       // re-trigger a product update webhook and infinite loop?
-      console.log(request)
       break
     default:
       throw new Response("Unhandled webhook topic", { status: 404 });
@@ -36,3 +39,14 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   throw new Response();
 };
+
+// TODO
+// - update shop product description updates table to have image url
+//    - So that when a product update webhook is triggered we can check to see if the image is diferent.
+//      If the image has not changed we don't need to run the webhook.
+// - add source field to shop product description updates to be webhook v bulk update
+// - add image url field to create calls when updates are triggered
+
+
+// TODO Questions
+// Can we trigger a google pubsub webhook locally to test?
